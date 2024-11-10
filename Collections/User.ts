b366dec779568eb3a3e3042c165185ad7f727aa3
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-10 19:00:46 +0530

    Add user collection

---
+++ Collections/User.ts
@@ -0,0 +1,37 @@
+import { Schema, Types} from "mongoose";
+import { IUser } from "../Types/types";
+import { config } from "../Config/Config";
+
+const userSchema = new Schema<IUser>({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    about:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    contact:{
+        type:String,
+        required:true
+    },
+    pets:[{
+        type:Schema.Types.ObjectId,
+        ref:'pets'
+    }]
+})
+
+export const UserModel = config.model<IUser>('users',userSchema);
+console.log("User Model created");
