8fef2e597662e204a71da0f1987645953884822a
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-10 19:16:15 +0530

    Update code with router

--- script.ts
+++ script.ts
@@ -1,10 +1,11 @@
+import { router } from './Routes/router';
 import express from 'express'
 import cors from 'cors'
 
 export const app = express();
 app.use(express.json());
 app.use(cors())
-
+app.use('/',router);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
