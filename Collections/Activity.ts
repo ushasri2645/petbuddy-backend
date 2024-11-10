7c9df12915245c80b89b35b605f1a838575f3b00
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-10 19:01:55 +0530

    Add activity collection

---
+++ Collections/Activity.ts
@@ -0,0 +1,34 @@
+import { Schema} from "mongoose";
+import { IActivity } from "../Types/types";
+import { config } from "../Config/Config";
+
+const activitySchema = new Schema<IActivity>({
+    title:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    startTime:{
+        type:Date,
+        required:true
+    },
+    endTime:{
+        type:Date,
+        required:true
+    },
+    status:{
+        type:Boolean,
+        required:true
+    },
+    reminder_id:{
+        type:Schema.Types.ObjectId,
+        ref:'reminders',
+        required:true
+    } 
+})
+    
+export const ActivityModel = config.model<IActivity>('activities',activitySchema);
+console.log("Activity model created");
