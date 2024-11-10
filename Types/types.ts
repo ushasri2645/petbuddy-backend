d0fcf6b3f4f23bc7a8dce95efd94de5a84b25965
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-10 19:21:40 +0530

    Add types file

---
+++ Types/types.ts
@@ -0,0 +1,78 @@
+import { Schema, Types } from "mongoose"
+
+export interface IServiceType extends Document{
+    name:string,
+    designation:string,
+    ratings:number,
+    no_of_reviews:number,
+    experience:number,
+    distance:number,
+    min_fee:number,
+    startDay:string
+    endDay:string,
+    startTime:string,
+    endTime:string
+    image_uri:string
+}
+
+
+
+export interface IBoardingServiceType extends Document{
+    name:string,
+    designation:string,
+    ratings:number,
+    no_of_reviews:number,
+    experience:number,
+    distance:number,
+    min_fee:number,
+    image_uri:string
+}
+
+export interface IServicesType{
+    grooming:IServiceType[],
+    boarding:IBoardingServiceType[],
+    training:IServiceType[],
+    veternity:IServiceType[],
+}
+export interface IReminder extends Document{
+    _id: any
+    title:string,
+    startTime:Date,
+    endTime:Date,
+    type:string,
+    date:Date
+}
+
+export interface IActivity extends Document{
+    title:string,
+    date:Date,
+    startTime:Date,
+    endTime:Date,
+    status:boolean,
+    reminder_id:Schema.Types.ObjectId
+}
+
+
+export interface IPet extends Document{
+    name:string,
+    breed:string,
+    gender:string,
+    age:number,
+    weight:number,
+    height:number,
+    color:string,
+    remarks?:string,
+    activities:Types.ObjectId[],
+    reminders:Types.ObjectId[],
+    gallery:string[]
+}
+
+export interface IUser extends Document{
+    name:string, 
+    password:string,
+    address:string,
+    about:string,
+    email:string,
+    contact:string
+    pets:Types.ObjectId[],
+}
