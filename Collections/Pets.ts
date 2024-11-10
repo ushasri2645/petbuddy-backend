cca053c84449ed974d40b8633fef0a62d9cd7245
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-10 19:02:43 +0530

    Add pet collection

---
+++ Collections/Pets.ts
@@ -0,0 +1,55 @@
+import { Schema} from "mongoose";
+import { config } from "../Config/Config";
+import { IPet } from "../Types/types";
+
+const petSchema = new Schema<IPet>({
+    name:{
+        type:String,
+        required:true
+    },
+    breed:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    weight:{
+        type:Number,
+        required:true
+    },
+    height:{
+        type:Number,
+        required:true
+    },
+    color:{
+        type:String,
+        required:true
+    },
+    remarks:{
+        type:String,
+    },
+    activities:[{
+        type:Schema.Types.ObjectId,
+        ref:'activities',
+        default:[]
+    }],
+    reminders:[{
+        type:Schema.Types.ObjectId,
+        ref:'reminders',
+        default:[]
+    }],
+    gallery:[{
+        type:String,
+        default:[]
+    }]
+})
+
+
+export const PetModel = config.model<IPet>('pets',petSchema)
+console.log("Pets Model Created");
