fb03d19c63ed60715bdd5f8478f098c437a2a0d1
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-14 02:18:14 +0530

    Update userUtils

--- Utils/userUtils.ts
+++ Utils/userUtils.ts
@@ -4,32 +4,39 @@ import { IUser } from "../Types/types";
 namespace UserUtils {
     export async function addUser(user: any) {
         try {
-            const newUser = await  UserModel.create(user);
-            // await newUser.save();
-            console.log(newUser)
+            const newUser = await UserModel.create(user);
+            console.log(newUser);
             return newUser;
         } catch (e) {
-            throw new Error(`Error creating user: ${e}`)
+            throw new Error(`Error creating user: ${e}`);
         }
     }
 
-    export async function validateUser(name:string,password:string){
-        try{
-            const user = await UserModel.findOne({name:name})
-            console.log(user)
-            if(!user){
-                return "Invalid User name."
+    export async function validateUser(name: string, password: string) {
+        try {
+            const user = await UserModel.findOne({ name: name });
+            console.log(user);
+            if (!user) {
+                return "Invalid User name.";
             }
-            if(user.password!=password){
-                return "Invalid Password"
+            if (user.password != password) {
+                return "Invalid Password";
+            } else {
+                return user;
             }
-            else{
-                return user
-            }
+        } catch (e) {
+            throw new Error(`Error Fetching details of user: ${e}`);
         }
-        catch(e){
-            throw new Error(`Error Fetching details of user: ${e}`)
-        }
+    }
+
+    export async function addProfile(name: string, profile: string) {
+        const user = await UserModel.findOneAndUpdate(
+            { name: name },
+            { $set: { image_uri: profile } },
+            { new: true, upsert: false }
+        );
+        console.log(`Profile image updated for user: ${name}`);
+        return user;
     }
 }
 
