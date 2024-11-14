15a0d16e62189d640619a9f9e5c6b6691f9470a8
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-15 00:34:14 +0530

    Update routes

--- Routes/router.ts
+++ Routes/router.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { userRouter } from "./User";
 import { serviceRouter } from "./Services";
 import { petRouter } from "./Pets";
+import { reminderRouter } from "./Reminders";
 export const router = express.Router();
 
 router.use('/api',userRouter);
 router.use('/api',serviceRouter)
 router.use('/api',petRouter)
+router.use('/api',reminderRouter)
