a7f51d67f295c80d75456c31dcc3d408265dc249
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-18 04:33:55 +0530

    Add activity router to main router

--- Routes/router.ts
+++ Routes/router.ts
@@ -3,9 +3,11 @@ import { userRouter } from "./User";
 import { serviceRouter } from "./Services";
 import { petRouter } from "./Pets";
 import { reminderRouter } from "./Reminders";
+import { activityRouter } from "./Activities";
 export const router = express.Router();
 
 router.use('/api',userRouter);
 router.use('/api',serviceRouter)
 router.use('/api',petRouter)
 router.use('/api',reminderRouter)
+router.use('/api',activityRouter)
