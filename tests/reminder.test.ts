9992148210b72ea260a2929963a304410965e50b
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-18 04:48:11 +0530

    Update tests

--- tests/reminder.test.ts
+++ tests/reminder.test.ts
@@ -14,267 +14,115 @@ app.use("/api", reminderRouter);
 jest.mock("../Collections/User");
 jest.mock("../Collections/Reminder");
 jest.mock("../Collections/Pets");
-// jest.mock("../Utils/reminderUtils");
 
-describe("Test for remidners utils", () => {
-    describe("Adding reminder Tests", () => {
-        beforeEach(() => {
-            jest.clearAllMocks();
-        });
-        const mockPet = {
-            _id: "pet123",
-            name: "Buddy",
-            reminders: [],
-            save: jest.fn(),
-        };
-        const mockReminder = {
-            _id: "1",
-            title: "Reminder1",
-            startTime: "8:00",
-            endTime: "9:00",
-        };
-        it("should not create reminder as pet not found", async () => {
+
+describe("reminderRouter Tests", () => {
+    const mockPet = {
+        _id: "pet123",
+        name: "Buddy",
+        reminders: [],
+        save: jest.fn(),
+    };
+
+    const mockReminder = {
+        _id: "reminder123",
+        title: "Reminder 1",
+        startTime: "8:00 AM",
+        endTime: "9:00 AM",
+    };
+
+    const mockUser = {
+        _id: "user123",
+        name: "Usha",
+        pets: [{ _id: "pet123" }],
+    };
+
+    describe("Tests for post request", () => {
+        it("should return 404 if pet not found", async () => {
             (PetModel.findOne as jest.Mock).mockResolvedValue(null);
-            const response = await request(app)
-                .post("/api/reminders/Buddy")
+
+            const res = await request(app)
+                .post("/api/pets/reminders/Buddy")
                 .send(mockReminder);
-            expect(response.status).toBe(404);
+
+            expect(res.status).toBe(404);
+            expect(res.text).toBe("No pet found");
         });
 
-        it("should create a reminder and associate it with the pet", async () => {
+        it("should add a reminder to a pet and return pet with reminder", async () => {
             (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
             (ReminderModel.create as jest.Mock).mockResolvedValue(mockReminder);
 
-            const response = await request(app)
+            const res = await request(app)
                 .post("/api/pets/reminders/Buddy")
                 .send(mockReminder);
 
-            expect(response.status).toBe(201);
-            expect(mockPet.reminders).toContain(mockReminder._id);
-            expect(mockPet.save).toHaveBeenCalled();
+            expect(res.status).toBe(201);
+            expect(res.body.reminders).toContain(mockReminder._id);
         });
 
-        it("should return 400 if an error occurs during reminder creation", async () => {
+        it("should return 400 if there is an error adding the reminder", async () => {
             (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
             (ReminderModel.create as jest.Mock).mockRejectedValue(
-                "Database Error"
+                new Error("Error")
             );
 
-            const response = await request(app)
+            const res = await request(app)
                 .post("/api/pets/reminders/Buddy")
                 .send(mockReminder);
 
-            expect(response.status).toBe(400);
-            expect(response.body).toEqual({ error: "Database Error" });
+            expect(res.status).toBe(400);
         });
     });
 
-    // describe("GET /pets/reminders/:name", () => {
-    //     beforeEach(() => {
-    //         jest.clearAllMocks();
-    //     });
-    //     const mockPet = {
-    //         _id: "pet123",
-    //         name: "Buddy",
-    //         reminders: [],
-    //         save: jest.fn(),
-    //     };
-    //     const mockReminder = {
-    //         _id: "1",
-    //         title: "Reminder1",
-    //         startTime: "8:00",
-    //         endTime: "9:00",
-    //     };
-    //     it("should return 404 if pet is not found", async () => {
-    //         (PetModel.findOne as jest.Mock).mockResolvedValue(null);
+    describe("GET /pets/reminders/:name", () => {
+        it("should return 404 if pet not found", async () => {
+            (PetModel.findOne as jest.Mock).mockResolvedValue(null);
 
-    //         const response = await request(app).get(
-    //             "/api/pets/reminders/Buddy"
-    //         );
+            const res = await request(app).get("/api/pets/reminders/Buddy");
 
-    //         expect(response.status).toBe(404);
-    //         expect(response.text).toBe("Pet not found");
-    //     });
-
-    //     it("should return reminders for the pet", async () => {
-    //         const mockReminders = [mockReminder];
-    //         (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
-    //         (reminderUtils.getReminders as jest.Mock).mockResolvedValue(
-    //             mockReminders
-    //         );
-
-    //         const response = await request(app).get(
-    //             "/api/pets/reminders/Buddy"
-    //         );
-
-    //         expect(response.status).toBe(200);
-    //         expect(response.body).toEqual(mockReminders);
-    //     });
-
-    //     it("should return 500 if an error occurs", async () => {
-    //         (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
-    //         (reminderUtils.getReminders as jest.Mock).mockRejectedValue(
-    //             "Error fetching reminders"
-    //         );
-
-    //         const response = await request(app).get(
-    //             "/api/pets/reminders/Buddy"
-    //         );
-
-    //         expect(response.status).toBe(500);
-    //         expect(response.text).toContain(
-    //             "Error fetching pet details: Error fetching reminders"
-    //         );
-    //     });
-    // });
-
-    // describe("GET /allReminders/:username", () => {
-    //     beforeEach(() => {
-    //         jest.clearAllMocks();
-    //     });
-    //     const mockPet = {
-    //         _id: "pet123",
-    //         name: "Buddy",
-    //         reminders: [],
-    //         save: jest.fn(),
-    //     };
-    //     const mockReminder = {
-    //         _id: "1",
-    //         title: "Reminder1",
-    //         startTime: "8:00",
-    //         endTime: "9:00",
-    //     };
-    //     it("should return all reminders for a user", async () => {
-            
-    //         const mockReminders = [
-    //             mockReminder,
-    //         ];
-    //         (reminderUtils.getAllReminders as jest.Mock).mockResolvedValue(
-    //             mockReminders
-    //         );
-
-    //         const response = await request(app).get(
-    //             "/api/allReminders/johndoe"
-    //         );
-
-    //         expect(response.status).toBe(200);
-    //         expect(response.body).toEqual(mockReminders);
-    //     });
-
-    //     it("should return 500 if an error occurs", async () => {
-    //         (reminderUtils.getAllReminders as jest.Mock).mockRejectedValue(
-    //             "Error fetching reminders"
-    //         );
-
-    //         const response = await request(app).get(
-    //             "/api/allReminders/johndoe"
-    //         );
-
-    //         expect(response.status).toBe(500);
-    //         expect(response.text).toContain("Error fetching reminders");
-    //     });
-    // });
-});
-
-describe('reminderRouter Tests', () => {
-    const mockPet = {
-      _id: "pet123",
-      name: "Buddy",
-      reminders: [],
-      save: jest.fn(),
-    };
-  
-    const mockReminder = {
-      _id: "reminder123",
-      title: "Reminder 1",
-      startTime: "8:00 AM",
-      endTime: "9:00 AM",
-    };
-  
-    const mockUser = {
-      _id: "user123",
-      name: "Usha",
-      pets: [{ _id: "pet123" }],
-    };
-  
-    describe('POST /pets/reminders/:name', () => {
-      it('should return 404 if pet not found', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValue(null);
-  
-        const res = await request(app)
-          .post('/api/pets/reminders/Buddy')
-          .send(mockReminder);
-  
-        expect(res.status).toBe(404);
-        expect(res.text).toBe("No pet found");
-      });
-  
-      it('should add a reminder to a pet and return pet with reminder', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
-        (ReminderModel.create as jest.Mock).mockResolvedValue(mockReminder);
-  
-        const res = await request(app)
-          .post('/api/pets/reminders/Buddy')
-          .send(mockReminder);
-  
-        expect(res.status).toBe(201);
-        expect(res.body.reminders).toContain(mockReminder._id);
-      });
-  
-      it('should return 400 if there is an error adding the reminder', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
-        (ReminderModel.create as jest.Mock).mockRejectedValue(new Error('Error'));
-  
-        const res = await request(app)
-          .post('/api/pets/reminders/Buddy')
-          .send(mockReminder);
-  
-        expect(res.status).toBe(400);
-      });
-    });
-  
-    describe('GET /pets/reminders/:name', () => {
-      it('should return 404 if pet not found', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValue(null);
-  
-        const res = await request(app).get('/api/pets/reminders/Buddy');
-  
-        expect(res.status).toBe(404);
-        expect(res.text).toBe("Pet not found");
-      });
-    it('should return pet reminders for a specific pet', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValueOnce(mockPet);
-        (PetModel.findOne as jest.Mock).mockReturnValueOnce({
-            populate: jest.fn().mockReturnValueOnce({
-                exec: jest.fn().mockResolvedValue(mockReminder),
-            }),
+            expect(res.status).toBe(404);
+            expect(res.text).toBe("Pet not found");
         });
-        const response = await request(app).get('/api/pets/reminders/Buddy');
-        expect(response.status).toBe(200);
-      });
-  
-      it('should return 500 if an error occurs fetching reminders', async () => {
-        (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
-        (ReminderModel.find as jest.Mock).mockRejectedValue(new Error('Error'));
-  
-        const res = await request(app).get('/api/pets/reminders/Buddy');
-  
-        expect(res.status).toBe(500);
-      });
-    });
-  
-    describe('GET /allReminders/:username', () => {
-        it('should return error while fetching all reminders', async () => {
-            (UserModel.findOne as jest.Mock).mockResolvedValueOnce(mockUser);
-            (PetModel.findById as jest.Mock).mockResolvedValueOnce(mockPet);
-        
+        it("should return pet reminders for a specific pet", async () => {
+            (PetModel.findOne as jest.Mock).mockResolvedValueOnce(mockPet);
             (PetModel.findOne as jest.Mock).mockReturnValueOnce({
                 populate: jest.fn().mockReturnValueOnce({
                     exec: jest.fn().mockResolvedValue(mockReminder),
                 }),
             });
-            const response = await request(app).get('/api/allReminders/Usha');
-            expect(response.status).toBe(500);
-          });
+            const response = await request(app).get(
+                "/api/pets/reminders/Buddy"
+            );
+            expect(response.status).toBe(200);
+        });
+
+        it("should return 500 if an error occurs fetching reminders", async () => {
+            (PetModel.findOne as jest.Mock).mockResolvedValue(mockPet);
+            (ReminderModel.find as jest.Mock).mockRejectedValue(
+                new Error("Error")
+            );
+
+            const res = await request(app).get("/api/pets/reminders/Buddy");
+
+            expect(res.status).toBe(500);
+        });
     });
-  });
+
+    describe("GET /allReminders/:username", () => {
+        it("should return error while fetching all reminders", async () => {
+            (UserModel.findOne as jest.Mock).mockResolvedValueOnce(mockUser);
+            (PetModel.findById as jest.Mock).mockResolvedValueOnce(mockPet);
+
+            (PetModel.findOne as jest.Mock).mockReturnValueOnce({
+                populate: jest.fn().mockReturnValueOnce({
+                    exec: jest.fn().mockResolvedValue(mockReminder),
+                }),
+            });
+            const response = await request(app).get("/api/allReminders/Usha");
+            expect(response.status).toBe(500);
+        });
+    });
+
+    
+});
