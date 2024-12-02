3dacb16b8953e0108e87a927d2943a39530a266a
ushasri2645 <ugudikandula@everest.engineering>
2024-12-02 10:26:49 +0530

    Update code handling not found case

--- Utils/reminderUtils.ts
+++ Utils/reminderUtils.ts
@@ -5,41 +5,49 @@ import { UserModel } from "../Collections/User";
 import { IActivity, IReminder } from "../Types/types";
 
 export namespace reminderUtils {
-    export async function getReminders(name: string) {
-        try {
-            const pet = await PetModel.findOne({ name });
-            if (!pet) {
-                return "Pet not found";
-            }
-            const petReminders = await PetModel.findOne({ name })
-                .populate("reminders")
-                .exec();
-            return petReminders?.reminders;
-        } catch (e) {
-            throw new Error(`${e}`);
-        }
+  export async function getReminders(name: string) {
+    try {
+      const pet = await PetModel.findOne({ name });
+      if (!pet) {
+        return "Pet not found";
+      }
+      const petReminders = await PetModel.findOne({ name })
+        .populate("reminders")
+        .exec();
+      if (!petReminders) {
+        return "Pet with reminder not found";
+      }
+      return petReminders.reminders;
+    } catch (e) {
+      throw new Error(`${e}`);
     }
+  }
 
-    export async function getAllReminders(name: string) {
-        try {
-            const user = await UserModel.findOne({ name });
-            const reminders: any[] = [];
-            const petIds = user?.pets;
-            if (petIds) {
-                for (const id of petIds) {
-                    const petRecord = await PetModel.findById(id);
-                    let reminder = await ReminderModel.find({
-                        petName: petRecord?.name,
-                    });
-                    console.log(reminder);
-                    if (reminder) {
-                        reminders.push(...reminder);
-                    }
-                }
+  export async function getAllReminders(name: string) {
+    try {
+      const user = await UserModel.findOne({ name });
+      const reminders: any[] = [];
+      if (!user) {
+        return "No user Found";
+      }
+      const petIds = user.pets;
+      if (petIds) {
+        for (const id of petIds) {
+          const petRecord = await PetModel.findById(id);
+          if (petRecord) {
+            let reminder = await ReminderModel.find({
+              petName: petRecord.name,
+            });
+            console.log(reminder);
+            if (reminder) {
+              reminders.push(...reminder);
             }
-            return reminders;
-        } catch (e) {
-            throw new Error(`$${e}`);
+          }
         }
+      }
+      return reminders;
+    } catch (e) {
+      throw new Error(`$${e}`);
     }
+  }
 }
