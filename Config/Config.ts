3eb1b5584af0afd8ea83516df20606166a9e42ee
ushasri2645 <ugudikandula@everest.engineering>
2024-12-03 11:00:14 +0530

    Update config using mongoose.connect

--- Config/Config.ts
+++ Config/Config.ts
@@ -1,13 +1,9 @@
-import mongoose, { Connection } from "mongoose";
-let config: Connection;
-export function connectToDatabase(): Connection {
-    try {
-        config = mongoose.createConnection("mongodb://localhost/petBuddyDB");
-        console.log("Connected");
-    } catch (error: any) {
-        throw new Error("Database connection failed: " + error.message);
-    }
-    return config;
+import mongoose from "mongoose";
+export async function connectToDatabase() {
+  try {
+    await mongoose.connect("mongodb://localhost/petBuddyDataBase");
+    console.log("Db created");
+  } catch (error: any) {
+    throw new Error("Database connection failed");
+  }
 }
-config = connectToDatabase();
-export {config}
