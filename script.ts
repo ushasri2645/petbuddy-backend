ff07dbb16f013253024f9e1a37f51258e920482e
Usha <160113438+ushasri2645@users.noreply.github.com>
2025-06-10 22:14:37 +0530

    Update script.ts

--- script.ts
+++ script.ts
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 8080;
 const runServer = async() =>{ 
   try{
     await connectToDatabase();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    })
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server on 0.0.0.0:${PORT}`));
   }
   catch(e){
     console.log("Error")
